e0acffc6b642194dbe9745cc1cbd1901dd81ea00
Kush Aggarwal <apple@Kushs-MacBook-Pro.local>
2024-03-31 22:44:34 +0530

    final

--- node.py
+++ node.py
@@ -52,7 +52,6 @@ class Node():
         self.lease_acquired = False
         self.lease_interval = 0 # see if needed here or not
         self.lease_duration = 0
-        # self.lease_timer2 = 
         self.dump_file_path = f"./logs_node_{self.id}/dump.txt"
         self.log_file_path = f"./logs_node_{self.id}/logs.txt"
         self.meta_file_path = f"./logs_node_{self.id}/metadata.txt"
@@ -68,11 +67,8 @@ class Node():
             dump_file.flush()
     
     def write_to_logs(self, term, op, key, value):
-        # print("In write_to_logs function")
         dump_text = f"{op} {key} {value} {term}"
         with open(self.log_file_path, 'a') as log_file:
-            # print("file opened to write logs")
-            # print(f"dump text: {dump_text}")
             log_file.write(dump_text + '\n')
             log_file.flush()
 
@@ -103,13 +99,6 @@ class Node():
     def start(self):
         self.timer_init()
         self.timer.start()
-    
-    # def timer_lease(self):
-    #     self.lease_time2 = rnd.randint(4,8)
-    #     self.lease_timer2 = Timer(self.lease_timer_interval, self.lease_finish)
-    
-    # def lease_finish(self):
-
 
     def timer_init(self):
         # self.timer_interval = rnd.randint(150,300) / 1000
@@ -122,22 +111,17 @@ class Node():
         self.lease_timer = Timer(self.lease_timer_interval, self.lease_over)
 
     def follower_lease_timer(self, interval):
-        # self.lease_timer_interval = rnd.randint(4,8)
-        # self.lease_over_time = datetime.now(utc_timezone) + timedelta(seconds = self.lease_timer_interval)
         self.lease_timer = Timer(interval, self.lease_over)
 
     def lease_over(self):
-        print("entered lease over")
         if self.state == State.FOLLOWER and self.status == Status.RUNNING:
-            print("Entered first if condition")
 
             if self.lease_timer.finished:
                 self.lease_interval = 0
-                print(f"Leader lease renewal failed, stepping down as leader")
                 self.become_follower()
         elif self.state == State.LEADER and self.status == Status.RUNNING:
             if self.lease_timer.finished:
-                print("Leader lease timed out stepping down")
+                self.print_and_write(f"Leader {self.id} lease renewal failed. Stepping Down") #dumpline 2
                 self.become_follower()
 
     
@@ -155,12 +139,10 @@ class Node():
                 self.start_election()
     
     def timer_reinit(self):
-        # self.timer_interval = rnd.randint(150,300) / 1000
         self.timer_interval = rnd.randint(5,9) 
 
     def timer_reset(self):
         self.timer.cancel()
-        # print(f"{self.id} timer got reset")
         self.timer = Timer(self.timer_interval, self.timer_follower)
         self.timer.start()
 
@@ -183,8 +165,7 @@ class Node():
     def become_candidate(self):
         self.update_state(State.CANDIDATE)
         self.update_term()
-        # print(f"Node {self.id} election timer timed out, Starting election")
-        self.print_and_write(f"Node {self.id} election timer timed out, Starting election")
+        self.print_and_write(f"Node {self.id} election timer timed out, Starting election.") #dumpline 4
         self.start_election()
     
     def start_election(self):
@@ -197,23 +178,20 @@ class Node():
         votes = [0 for i in range(len(self.neighbours))]
         for i in self.neighbours:
             if i.id == self.id:
-                # self.print_and_write("Voted for myself")
                 votes[i.id] = 1
                 pass
             else:
                 thread = Thread(target = self.request_votes, args = (i, votes))
                 requests.append(thread)
                 thread.start()
-                # self.request_votes(i, votes)
 
         for i in requests: 
             i.join()
-        print(votes)
         if self.state != State.CANDIDATE:
             return
         if sum(votes) > len(votes)//2:
 
-            self.print_and_write(f"Node {self.id} becomes leader for term {self.term}, waiting for old lease to run out")
+            self.print_and_write("New Leader waiting for Old Leader Lease to timeout.") #dumpline 3
             time.sleep(self.lease_interval)
             self.lease_interval= 0
             self.become_leader()
@@ -241,14 +219,11 @@ class Node():
             cLogTerm = cLogTerm)
 
             response = stub.RequestVote(request)
-            # print("here")
             if response.term > self.term:
-                print(self.lease_interval)
-                self.lease_interval = max(self.lease_interval, request.interval) # need to see if this is correct but should not matter
+                self.lease_interval = max(self.lease_interval, request.interval)
                 self.update_terms(response.term)
                 self.become_follower()
             elif response.status == True and self.term >= response.term:
-                print(self.lease_interval, "hello")
                 self.lease_interval = max(self.lease_interval, response.interval)
                 votes[i.id] = 1
         except Exception as e: 
@@ -262,40 +237,32 @@ class Node():
     def become_leader(self):
         if self.status == Status.CRASHED:
             return
-        # self.nextIndex = [len(self.log_table)]* len(self.neighbours)
-        # self.matchIndex = [0]  * len(self.neighbours)
-        # self.leader_id = self.id
 
         if self.state == State.CANDIDATE:
-            self.print_and_write(f"Node {self.id} Became Leader and renewing lease")
+            self.print_and_write(f"Node {self.id} became the leader for term {self.term}.") #dumpline 5
             self.lease_acquired = True
             self.update_state(State.LEADER)
             self.voted_for= self.id
-            print(self.term)
             self.nextIndex = [len(self.log_table)]* len(self.neighbours)
             self.matchIndex = [0]  * len(self.neighbours)
-            # print("new leader match index: ", self.matchIndex)
             entry = {
                 'term': self.term,
                 'update': ['NO OP', "", ""]
             }
             self.log_table.append(entry)
             self.leader_id = self.id
-            # self.lease_timer_init()
             self.heartbeat_timer()
-            # print("Test2")
 
     def send_heartbeat(self, addr):
         if self.status == Status.CRASHED:
             return
         
-        self.print_and_write("Leader sending heartbeat and renewing lease")
+        self.print_and_write(f"Leader {self.id} sending heartbeat & Renewing Lease") #dumpline 1
         channel = grpc.insecure_channel(f"{addr.ip}:{addr.port}")
         stub = raft_pb2_grpc.RaftStub(channel)
         request= {}
         self.lease_timer_init()
         propInterval = self.lease_timer_interval
-        # lease_time_str = self.lease_over_time.isoformat()
         # Log Replication
 
         prefixLen= self.nextIndex[addr.id]
@@ -306,7 +273,6 @@ class Node():
 
         appending_entries= self.log_table[prefixLen:]
         if self.nextIndex[addr.id]<=len(self.log_table):
-            # print("went in this request line 230")
             request = raft_pb2.LogRequest(leaderID = self.id,leaderTerm = self.term,
                             prefixLen = prefixLen,
                             prefixTerm = prefixTerm,
@@ -323,17 +289,14 @@ class Node():
                             lease_over_time = propInterval)
 
         try:
-            # print(f"Sent commit index: {self.commitIndex}")
             response = stub.AppendEntries(request)
             if response.term==self.term and self.state==State.LEADER:
                 if response.status==True:
                     self.flag_count+= 1
                 if response.status==True and response.ack>=self.matchIndex[response.nodeID]:
                     self.nextIndex[response.nodeID]= response.ack
-                    self.matchIndex[response.nodeID]= response.ack #BIG CHANGE HERE
-                    # print("WILL COMMIT HERE")
+                    self.matchIndex[response.nodeID]= response.ack
                     self.commit_function(response.term)
-                    # COMMIT HERE
                 
                 elif self.nextIndex[response.nodeID]>0:
                     self.nextIndex[response.nodeID]= self.nextIndex[response.nodeID]-1
@@ -341,9 +304,10 @@ class Node():
             elif response.term>self.term:
                 self.update_term= response.term
                 self.voted_for= -1
+                self.print_and_write(f"{self.id} Stepping down") #dumpline 14
                 self.become_follower()
         except:
-            self.print_and_write(f"Error while sending RPC to {addr.id} ")
+            self.print_and_write(f"Error occurred while sending RPC to Node {addr.id}.") #dumpline 6
             pass
     
     def heartbeat_timer(self):
@@ -364,22 +328,16 @@ class Node():
             t.join()
 
         if self.flag_count<majority:
-            print("aaaaaaaaaa")
             pass
-            # print("Stepping down due to less followers")
-            # self.become_follower()
         else:
-            print("Going good")
             self.flag_count= 1
         
-            # self.leader_timer.cancel()
             self.leader_timer = Timer(3, self.heartbeat_timer)
             self.leader_timer.start() # leader lease comes here
             self.lease_timer_init()
     
     def acks(self, len):
         count= 0
-        # print(f"in acks function {self.matchIndex} and input len {len}")
         for i in self.matchIndex:
             if i>=len:
                 count+= 1
@@ -388,32 +346,22 @@ class Node():
     
     def commit_function(self, term):
         with lock:
-            # print("Akshansh 1")
             minAcks= (len(self.neighbours)+1)//2
             ready= -1
-            # print(f"log table len in commit function: {len(self.log_table)}")
             for i in range(1, len(self.log_table)+1):
-                # print("test")
                 if self.acks(i)>=minAcks:
                     ready= i
-                # kush akshansh if this is not true should we fail to renew leader lease?
             
             if ready!=-1 and ready>self.commitIndex and self.log_table[ready-1]['term']==term:
                 for i in range(self.commitIndex, ready):
-                    # print("do something here to complete the code")
+                    operation= self.log_table[i]['update'][0]
                     key= self.log_table[i]['update'][1]
                     value= self.log_table[i]['update'][2]
                     self.applied_entries[key]= value
-                    self.write_to_logs(self.log_table[i]['term'], self.log_table[i]['update'][0], 
-                                        key, value)
-                    if self.state == State.LEADER:
-                        self.print_and_write(f"Node {self.id} (Leader) committed {self.log_table[i]['update'][0]} {self.log_table[i]['update'][1]} {self.log_table[i]['update'][2]}  to state machine")
-                    else:
-                        self.print_and_write(f"Node {self.id} (Follower) committed {self.log_table[i]['update'][0]} {self.log_table[i]['update'][1]} {self.log_table[i]['update'][2]} entry to state machine")
+                    self.write_to_logs(self.log_table[i]['term'], operation, key, value)
+                    self.print_and_write(f"Node {self.id} (Leader) committed {operation} {key} {value}  to state machine") #dumpline 9
                 self.commitIndex= ready
-                # self.lastApplied            
     def signal_handler(self,sig, frame):
-        print('You pressed Ctrl+C! Node iD:',self.id)
         self.metadata_writer()
         os._exit(0)
 
@@ -442,10 +390,10 @@ class RaftHandler(raft_pb2_grpc.RaftServicer, Node):
         logOk = (cLogTerm > lastTerm) or (cLogTerm == lastTerm and cLogLength >= len(self.log_table))
         if cTerm == self.term and logOk and (self.voted_for == -1 or self.voted_for == cid):
             self.voted_for = cid
-            self.print_and_write(f"Voted for {cid} in term {cTerm}")
+            self.print_and_write(f"Vote granted for Node {cid} in term {cTerm}.") #dumpline 12
             return raft_pb2.VoteResponse(status = True, term = self.term, nodeID = cid, interval = self.lease_interval)
         else:
-            self.print_and_write(f"Voted denied for {cid} in term {cTerm}")
+            self.print_and_write(f"Vote denied for Node {cid} in term {cTerm}.") #dumpline 13
             return raft_pb2.VoteResponse(status = False, term = self.term, nodeID = cid, interval = self.lease_interval)
 
     def AppendEntries(self, request, context):
@@ -459,9 +407,6 @@ class RaftHandler(raft_pb2_grpc.RaftServicer, Node):
         suffix= request.entries
         lease_interval = request.lease_over_time
         self.follower_lease_timer(lease_interval)
-        # lease_over_time = datetime.fromisoformat(lease_over_time_str)
-        # self.lease_interval = (lease_over_time - datetime.now(utc_timezone)).total_seconds()
-        # self.print_and_write(f"{term} and {self.term}")
         if term>self.term:
             self.update_terms(term)
             self.voted_for= -1
@@ -474,10 +419,9 @@ class RaftHandler(raft_pb2_grpc.RaftServicer, Node):
         if term==self.term and logOk:
             self.actualAppendEntries(prefixLen, leaderCommit, suffix)
             ack= prefixLen+len(suffix)
-            self.print_and_write(f"Node {self.id} accepted append entries RPC")
             return raft_pb2.AppendEntriesResponse(status= True, ack= ack, nodeID= self.id, term= self.term)
         else:
-            self.print_and_write(f"Node {self.id} rejected append entries RPC")
+            self.print_and_write(f"Node {self.id} rejected AppendEntries RPC from {self.leader_id}.") #dumpline 11 
             return raft_pb2.AppendEntriesResponse(status= False, ack= 0, nodeID= self.id, term= self.term)
         
 
@@ -497,15 +441,18 @@ class RaftHandler(raft_pb2_grpc.RaftServicer, Node):
                     'term': suffix[i].term,
                     'update': suffix[i].update
                 }
+                self.print_and_write(f"Node {self.id} accepted AppendEntries RPC from {self.leader_id}.") #dumpline 10
                 self.log_table.append(entry)
 
         if leaderCommit>self.commitIndex:
             self.commitIndex= min(leaderCommit, self.last_len)
             while leaderCommit>self.commitIndex:
+                operation = self.log_table[self.commitIndex]['update'][0]
                 key= self.log_table[self.commitIndex]['update'][1]
                 value= self.log_table[self.commitIndex]['update'][2]
                 self.applied_entries[key]= value
-                self.write_to_logs(self.log_table[self.commitIndex]['term'], self.log_table[self.commitIndex]['update'][0], key, value)
+                self.write_to_logs(self.log_table[self.commitIndex]['term'], operation, key, value)
+                self.print_and_write(f"Node {self.id} (follower) committed the entry {operation} {key} {value} to the state machine.") #dumpline 7
                 self.commitIndex+= 1
             self.commitIndex= leaderCommit
             self.last_len = leaderCommit
@@ -515,13 +462,12 @@ class RaftHandler(raft_pb2_grpc.RaftServicer, Node):
             key = request.key
             value = request.value
             if self.state == State.LEADER and self.lease_acquired == True:
-                self.print_and_write(f"Node {self.id} received Set Request")
+                self.print_and_write(f"Node {self.id} (leader) received a Set {key} {value} request.") #dumpline 8
                 entry = {
                     'term': self.term,
                     'update': ['set', key, value]
                 }
                 self.log_table.append(entry)
-                print(len(self.log_table))
                 response = {
                     'success': True
                 }
@@ -535,7 +481,7 @@ class RaftHandler(raft_pb2_grpc.RaftServicer, Node):
     def GetValue(self, request, context):
         key = request.key
         if self.state == State.LEADER and self.lease_acquired == True:
-            self.print_and_write(f"Node {self.id} received Get Request")
+            self.print_and_write(f"Node {self.id} (leader) received a Get {key} request.") #dumpline 8
             if self.applied_entries[key]:
                 response = {
                     'success': True,
@@ -556,8 +502,6 @@ def run(handler: RaftHandler):
     server.add_insecure_port(f'[::]:{handler.address.port}')
     server.start()
     signal.signal(signal.SIGINT, handler.signal_handler)
-    print('Press Ctrl+C')
-    # signal.pause()
     server.wait_for_termination()
 
 
